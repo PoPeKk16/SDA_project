2923e582aeedc33eab9df04ad208f0e41d84454b
Piotrek <peter96@poczta.onet.pl>
2022-08-28 14:36:08 +0200

    Make login screen

--- Mooball/Mooball/settings.py
+++ Mooball/Mooball/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "Mooball.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -133,3 +133,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+LOGIN_REDIRECT_URL = "main"
+LOGOUT_REDIRECT_URL = "main"
--- Mooball/Mooball/urls.py
+++ Mooball/Mooball/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include('shop.urls')),
+    path('', TemplateView.as_view(template_name='main.html'), name='main')
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
